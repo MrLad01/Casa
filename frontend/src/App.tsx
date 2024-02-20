8b0f8554910e903b6f8c5ecf0875b782958234e2
MrLad01 <ladayodeji@gmail.com>
2024-02-20 23:04:39 +0100

    commencement of nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,31 @@
-function App() {
+import logo from '../src/assets/OWDN6985.jpg';
+// import  Link  from 'react-router-dom';
 
+function App() {
   return (
     <>
-
+      <div className = " bg-white h-screen w-screen">
+        <nav className = "flex justify-between w-full p-5">
+          <div className = 'flex items-center gap-4'>
+            <img src = { logo } alt="" className = 'w-10 h-10' />
+            <h3> CasaSwap </h3>
+          </div>
+          <div className = ' flex items-center gap-6' >
+            <a href = '/'>
+              Pricing
+            </a>
+            <a href = "/">
+              About Us
+            </a>
+            <button>
+              LOG IN
+            </button>
+            <button>
+              REGISTER FOR FREE
+            </button>
+          </div>
+        </nav>
+      </div>
     </>
   )
 }
